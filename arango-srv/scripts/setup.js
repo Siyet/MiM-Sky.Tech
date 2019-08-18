c582f0d0974ac11eb69e9fa63ccd91ce21a10fc6
Vlad Bedilo <v.overdrive@gmail.com>
2019-08-18 05:39:24 +0300

    add more startup

--- arango-srv/scripts/setup.js
+++ arango-srv/scripts/setup.js
@@ -7,3 +7,15 @@ if (!db._collection('helipads')) {
 if (!db._collection('helicopter')) {
     db._createDocumentCollection('helicopter');
 }
+
+if (!db._collection('forbidden_zone')) {
+    db._createDocumentCollection('forbidden_zone');
+}
+
+if (!db._collection('order')) {
+    db._createDocumentCollection('order');
+}
+
+if (!db._collection('route')) {
+    db._createDocumentCollection('route');
+}
